3169bff59f69ac09f1505392bc2325c9ff411e6b
Kevin Damen <k.damen@bellt.nl>
2023-07-28 09:03:56 +0200

    Add SqsPublisher

---
+++ SQS/SqsPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using SqsPublisher;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Text.Json;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "kevin.d.damen@gmail.com",
+    FullName = "Kevin Damen",
+    DateOfBirth = new DateTime(1996, 12, 3),
+    GitHubUsername = "GamiDroid"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
