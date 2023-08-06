9e7deca4a5060975cc6ae1c6636fa22e9b8b839b
Kevin Damen <k.damen@bellt.nl>
2023-08-06 11:55:30 +0200

    Refactor the consumer

--- SQS/Customers.Consumer/QueueConsumerService.cs
+++ SQS/Customers.Consumer/QueueConsumerService.cs
@@ -2,6 +2,7 @@
 using Amazon.SQS;
 using Amazon.SQS.Model;
 using Customers.Consumer.Messages;
+using MediatR;
 using Microsoft.Extensions.Options;
 
 namespace Customers.Consumer;
@@ -10,11 +11,15 @@ public class QueueConsumerService : BackgroundService
 {
     private readonly IAmazonSQS _sqs;
     private readonly IOptions<QueueSettings> _queueSettings;
+    private readonly IMediator _mediator;
+    private readonly ILogger<QueueConsumerService> _logger;
 
-    public QueueConsumerService(IAmazonSQS sqs, IOptions<QueueSettings> queueSettings)
+    public QueueConsumerService(IAmazonSQS sqs, IOptions<QueueSettings> queueSettings, IMediator mediator, ILogger<QueueConsumerService> logger)
     {
         _sqs = sqs;
         _queueSettings = queueSettings;
+        _mediator = mediator;
+        _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -29,22 +34,29 @@ public class QueueConsumerService : BackgroundService
             MaxNumberOfMessages = 1
         };
 
-        while(stoppingToken.IsCancellationRequested)
+        while(!stoppingToken.IsCancellationRequested)
         {
             var response = await _sqs.ReceiveMessageAsync(receiveMessageRequest, stoppingToken);
             foreach (var message in response.Messages)
             {
                 var messageType = message.MessageAttributes["MessageType"].StringValue;
-
-                switch (messageType)
+                var type = Type.GetType($"Customers.Consumer.Messages.{messageType}");
+                if (type is null)
                 {
-                    case nameof(CustomerCreated):
-                        var created = JsonSerializer.Deserialize<CustomerCreated>(message.Body);
-                        break;
-                    case nameof(CustomerUpdated): 
-                        break;
-                    case nameof(CustomerDeleted): 
-                        break;
+                    _logger.LogWarning("Unknown message type: {MessageType}", messageType);
+                    continue;
+                }
+
+                var typedMessage = (ISqsMessage)JsonSerializer.Deserialize(message.Body, type)!;
+
+                try
+                {
+                    await _mediator.Send(typedMessage, stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Message failed during processing");
+                    continue;
                 }
 
                 await _sqs.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle, stoppingToken);
