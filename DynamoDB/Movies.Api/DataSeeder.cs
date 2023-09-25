fc626201733dae023830a2110e5967a346aaca03
Kevin Damen <k.damen@bellt.nl>
2023-09-25 19:02:06 +0200

    Implemented demo for dynamoDB transactions

--- DynamoDB/Movies.Api/DataSeeder.cs
+++ DynamoDB/Movies.Api/DataSeeder.cs
@@ -26,7 +26,7 @@ public class DataSeeder
             var ageRestriction = int.Parse(commaSplit[2]);
             var rottenTomatoes = int.Parse(commaSplit[3]);
 
-            var movie = new Movie
+            var movie = new Movie1
             {
                 Id = Guid.NewGuid(),
                 Title = title,
@@ -41,7 +41,7 @@ public class DataSeeder
 
             var createItemRequest = new PutItemRequest
             {
-                TableName = "movies",
+                TableName = "movies-year-title",
                 Item = itemAsAttributes
             };
 
--- DynamoDB/Movies.Api/Program.cs
+++ DynamoDB/Movies.Api/Program.cs
@@ -1,3 +1,57 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
 using Movies.Api;
+using System.Text.Json;
 
-await new DataSeeder().ImportDataAsync();
+//await new DataSeeder().ImportDataAsync();
+
+var newMovie1 = new Movie1
+{
+    Id = Guid.NewGuid(),
+    Title = "21`Jump Street",
+    AgeRestriction = 18,
+    ReleaseYear  = 2012,
+    RottenTomatoesPercentage = 85,
+};
+
+var newMovie2 = new Movie2
+{
+    Id = Guid.NewGuid(),
+    Title = "21`Jump Street",
+    AgeRestriction = 18,
+    ReleaseYear  = 2012,
+    RottenTomatoesPercentage = 85,
+};
+
+var asJson1 = JsonSerializer.Serialize(newMovie1);
+var attributeMap1 = Document.FromJson(asJson1).ToAttributeMap();
+
+var asJson2 = JsonSerializer.Serialize(newMovie2);
+var attributeMap2 = Document.FromJson(asJson2).ToAttributeMap();
+
+var transactionRequest = new TransactWriteItemsRequest
+{
+    TransactItems = new List<TransactWriteItem>
+    {
+        new()
+        {
+            Put = new()
+            {
+                TableName = "movies-year-title",
+                Item = attributeMap1
+            }
+        },
+        new()
+        {
+            Put = new()
+            {
+                TableName = "movies-title-rotten",
+                Item = attributeMap2
+            }
+        }
+    }
+};
+
+var dynamoDb = new AmazonDynamoDBClient();
+var response = await dynamoDb.TransactWriteItemsAsync(transactionRequest);
